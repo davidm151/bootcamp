a4c3d790feaeb541aa6fca76b8033d4c92c3d33a
David Marsal <lunbina2000@gmail.com>
2021-09-15 16:53:41 +0200

    algo

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Mensaje from './mensaje';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje message="bones"></Mensaje>
+      <Mensaje message="bones 2"></Mensaje>
+      <Mensaje message="bones 3"></Mensaje>
     </div>
   );
 }
